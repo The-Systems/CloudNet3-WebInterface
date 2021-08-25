a0ab29045d482c6dcc3efc439e9c9fc84f7818a7
Moritz Walter <32977558+Nevercold@users.noreply.github.com>
2021-08-25 21:34:07 +0200

    Update small-header.php

--- pages/small-header.php
+++ pages/small-header.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="/assets/styles.css" rel="stylesheet">
-    <script src="/assets/js/charts-ram.js" defer></script>
-    <script src="/assets/js/charts-cpu.js" defer></script>
+    <link href="<?= \webinterface\main::getUrl(); ?>/assets/styles.css" rel="stylesheet">
+    <script src="<?= \webinterface\main::getUrl(); ?>/assets/js/charts-ram.js" defer></script>
+    <script src="<?= \webinterface\main::getUrl(); ?>/assets/js/charts-cpu.js" defer></script>
 </head>
 <body class="dark:bg-gray-900 bg-gray-100">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
